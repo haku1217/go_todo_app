bd882012fc6e5a3f592530505e1d83d1ef060a99
Takahahsi Haruki <haruki12.17birthday@gmail.com>
2022-11-09 20:41:51 +0900

    add test and add make command

--- config/config_test.go
+++ config/config_test.go
@@ -1,33 +1,50 @@
 package config
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/caarlos0/env/v6"
 )
 
 func Test_New(t *testing.T) {
-	wantPort := 3333
-	t.Setenv("PORT", fmt.Sprint(wantPort))
+	t.Run("正常系", func(t *testing.T) {
+		wantPort := 3333
+		t.Setenv("PORT", fmt.Sprint(wantPort))
 
-	got, err := New()
-	if err != nil {
-		t.Fatalf("cannot create confg: %v", err)
-	}
+		wantEnv := "dev"
+		t.Setenv("TODO_ENV", fmt.Sprint(wantEnv))
 
-	r, err := json.Marshal(got)
+		got, err := New()
+		if err != nil {
+			t.Fatalf("cannot create confg: %v", err)
+		}
+		if got.Port != wantPort {
+			t.Errorf("want %d, but %d", wantPort, got.Port)
+		}
+		if got.Env != wantEnv {
+			t.Errorf("want %s, but %s", wantEnv, got.Env)
+		}
+	})
 
-	if err != nil {
-		t.Log("[DEBUG] fail Marshal")
-	}
-	t.Logf("[DEBUG] got is: %s", string(r))
+	t.Run("異常系", func(t *testing.T) {
+		old := envParse
+		defer func() { envParse = old }()
 
-	if got.Port != wantPort {
-		t.Errorf("want %d, but %d", wantPort, got.Port)
-	}
+		wantErr := errors.New("env.Parse error")
+		envParse = func(v interface{}, opts ...env.Options) error {
+			return wantErr
+		}
 
-	wantEnv := "dev"
-	if got.Env != wantEnv {
-		t.Errorf("want %s, but %s", wantEnv, got.Env)
-	}
+		got, err := New()
+
+		if err != wantErr {
+			t.Errorf("want %s, but %s", wantErr, err)
+		}
+
+		if got != nil {
+			t.Errorf("want nil, but %v", got)
+		}
+	})
 }
